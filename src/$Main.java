de133b1ce685143de556d5756731062b51749cc2
weikun <wk2003119@163.com>
2016-10-21 20:15:17 +0800

    102133

---
+++ src/$Main.java
@@ -0,0 +1,23 @@
+/**
+       author：卫昆
+       date:2016-10-21
+       心情：
+    */
+public class $Main {
+    /**
+     *
+     * @param args:形参
+     */
+    public static void main(String[] args) {
+        int 年龄=100;
+        int 岁数=100;//pascal
+        int c='卫';
+        int c2='崑';
+        char c1=20320;
+        System.out.print(c2);//单行注释
+
+
+
+    }
+
+}
